a7f749487b5f49ba6bc298ac16cf11579b76f133
avinashuppada6 <avinashfacts8@gmail.com>
2024-12-08 17:55:23 +0530

    images added

--- yogaforlife/src/app/app-routing.module.ts
+++ yogaforlife/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { ExpertguidanceComponent } from './expertguidance/expertguidance.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 const routes: Routes = [
   {path:'',component:WelcomeComponent},
   {path:'contactus',component:ContactusComponent},
+  {path:'aboutus',component:AboutusComponent},
   {path:'expertguidance',component:ExpertguidanceComponent},
   {path:'**',component:PagenotfoundComponent}
 ]
